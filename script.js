bef7a5305030b7c0721b56883d43832ea41667fc
Kothakapu Pooja <poojareddy5588@gmail.com>
2026-09-21 12:51:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let blockId = document.getElementById("block_id")
+let colourId = document.getElementById("colour_id")
+let changeBtn = document.getElementById("change_button")
+let resetBtn = document.getElementById("reset")
+
+let gridItems = document.querySelectorAll(".grid-item")
+
+changeBtn.addEventListener("click", () => {
+	gridItems.forEach(function (item) {
+		item.style.backgroundColor = "transparent"
+	})
+	let id = blockId.value
+	let selectedBlock = document.getElementById(id)
+
+	if(selectedBlock) {
+		selectedBlock.style.backgroundColor = colourId.value
+	}
+})
+	resetBtn.addEventListener("click",function () {
+		gridItems.forEach(function (item) {
+			item.style.backgroundColor = "transparent"
+		})
+	blockId.value = ""
+	colourId.value = ""
+	})
